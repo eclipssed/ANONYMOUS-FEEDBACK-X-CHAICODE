010eefe0c7110c954f9714e95d54e56f698ea43a
eclipssed <your_email@example.com>
2024-04-20 21:16:13 +0500

    added verify code route.

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,13 +1,59 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/user.model";
-import { z } from "zod";
-import { usernameValidation } from "@/schemas/signupSchema";
-
 
 export async function POST(request: Request) {
   await dbConnect();
   try {
-   const {username, code} = await request.json()
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "User is successfully verified.",
+        },
+        {
+          status: 200,
+        }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message:
+            "The verification code has expired, please sign up again to get a new verification code.",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return Response.json(
+        {
+          success: false,
+          message: "Invalid verification code.",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
   } catch (error) {
     console.error("Error verifying code.", error);
     return Response.json(
