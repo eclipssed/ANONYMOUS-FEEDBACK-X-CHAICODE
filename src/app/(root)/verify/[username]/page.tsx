001339527f4ceb0004803142dc7aa173741dd955
eclipssed <your_email@example.com>
2024-05-03 09:27:45 +0500

    added verifycode page

---
+++ src/app/(root)/verify/[username]/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { verifySchema } from "@/schemas/verifySchema";
+import { apiResponse } from "@/types/apiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useParams, useRouter } from "next/navigation";
+import {  useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const verifyPage = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const response = await axios.post("/api/verify-code", {
+        username: params.username,
+        code: data.code,
+      });
+      if (response.data.success) {
+        toast({
+          title: "Success",
+          description: response.data.message,
+        });
+        router.replace("/signin");
+      } else {
+        toast({
+          title: "Failed",
+          description: response.data.message,
+          variant: "destructive",
+        });
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<apiResponse>;
+      toast({
+        title: "Verification Failed",
+        description:
+          axiosError.response?.data.message ??
+          "An error occurred. Please try again.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+
+  return (
+    <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+          Verify Your Account
+        </h1>
+        <p className="mb-4">Enter the verification code sent to your email</p>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            name="code"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Verification Code</FormLabel>
+                <Input {...field} />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Verify</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default verifyPage;
--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,5 +1,5 @@
 import {z } from 'zod'
 
-export const verifyShema = z.object({
+export const verifySchema = z.object({
     code: z.string().length(6, {message: "verification code must be atleast 6 digits."})
 })
